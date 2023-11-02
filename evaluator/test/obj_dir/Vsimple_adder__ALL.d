421646a6c1afbbdf86d144848f5919b2a4b7a72f
- <->
2023-11-02 14:16:52 -0400

    Delete obj_dir

--- evaluator/test/obj_dir/Vsimple_adder__ALL.d
+++
@@ -1,7 +0,0 @@
-Vsimple_adder__ALL.o: Vsimple_adder__ALL.cpp Vsimple_adder.cpp \
- Vsimple_adder.h /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h Vsimple_adder__Syms.h \
- Vsimple_adder__Trace.cpp /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h Vsimple_adder__Slow.cpp \
- Vsimple_adder__Syms.cpp Vsimple_adder__Trace__Slow.cpp
